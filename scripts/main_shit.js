69cd9f90a133fd457606b375e25a77b9bc9d9e80
Sir Francis Billard <sirfrancisbillard@gmail.com>
2018-12-23 20:46:30 -1000

    Update main_shit.js

--- scripts/main_shit.js
+++ scripts/main_shit.js
@@ -5,7 +5,7 @@ var MuhSnogs = ["cc", "chickprod_v6", "geico_v3", "gta45_v5beta"];
 function GibsMeSnog()
 {
 	var MuhSnog = MuhSnogs[Math.floor(Math.random() * MuhSnogs.length)];
-	return "assets/" + MuhSong + ".mp3";
+	return "assets/" + MuhSnog + ".mp3";
 }
 
 function PlayzusMezusMuhSnogNShit()
